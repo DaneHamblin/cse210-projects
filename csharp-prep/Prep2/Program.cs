a898c670a771a0547079df5d4c5266609f4c7de1
DaneHamblin <ham19031@byui.edu>
2026-01-09 18:44:10 -0700

    Week 1 Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Net.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Hello, please enter your grade percentage to receive a letter grade. ");
+        string input = Console.ReadLine();
+
+        if (float.TryParse(input, out float grade_percentage))
+            {
+                Console.WriteLine($"You entered: {grade_percentage}%");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        if (grade_percentage >= 90)
+            {
+                Console.Write("You got an A! ");
+            }
+            else if (grade_percentage >= 80)
+                {
+                    Console.Write("You got a B, ");
+                }
+                else if (grade_percentage >= 70)
+                    {
+                        Console.Write("You got a C, ");
+                    }
+                    else if (grade_percentage >= 60)
+                        {
+                            Console.Write("You got a D, ");
+                        }
+                        else if (grade_percentage < 60)
+                            {
+                                Console.Write("You got an F, ");
+                            }
+        if (grade_percentage >= 70)
+            {
+                Console.Write("You Pass!");
+            }
+            else if (grade_percentage <70)
+                {
+                    Console.Write("You Fail.");
+                }
     }
 }
